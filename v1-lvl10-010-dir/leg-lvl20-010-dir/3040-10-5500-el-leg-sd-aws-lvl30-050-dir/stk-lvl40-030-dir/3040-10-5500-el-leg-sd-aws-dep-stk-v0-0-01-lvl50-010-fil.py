88a65a316359c8ed1a7b6962ce94e76eabc1a874
brown.point <brown.point@sambar.one>
2023-07-03 06:57:57 +0000

    Added new files with pakages. Original ticket: sd304010-22 - 1041.

--- v1-lvl10-010-dir/leg-lvl20-010-dir/3040-10-5500-el-leg-sd-aws-lvl30-050-dir/stk-lvl40-030-dir/3040-10-5500-el-leg-sd-aws-dep-stk-v0-0-01-lvl50-010-fil.py
+++ v1-lvl10-010-dir/leg-lvl20-010-dir/3040-10-5500-el-leg-sd-aws-lvl30-050-dir/stk-lvl40-030-dir/3040-10-5500-el-leg-sd-aws-dep-stk-v0-0-01-lvl50-010-fil.py
@@ -1,3 +1,11 @@
 {
-  "resourceMetadataKEY": "no"
+ "resourceMetadataKEY": "yes",
+ "botStakeKEY": "pyt",
+ "containerKEY": "id2",
+ "osKEY": "ubuntu 20.04",
+ "npmKEY": "8.09",
+ "nodeKEY": "16.09",
+ "awsCliKEY": "2.02.1",
+ "pyttKEY": "3.08",
+ "kubeCtlKEY": "2.21"
 }
